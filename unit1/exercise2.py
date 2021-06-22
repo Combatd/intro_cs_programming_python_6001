c1ab1aa1083c5d9482e6aaa576b6ace6b49dc3e8
Combatd <combatdstar@gmail.com>
2021-06-21 20:33:18 -0700

    Print out ranges and reverse ranges of strings

---
+++ unit1/exercise2.py
@@ -0,0 +1,19 @@
+str1 = 'hello'
+str2 = ','
+str3 = 'world'
+str4 = str1 + str3
+
+print(str1)
+print(str2)
+print(str3)
+print(str1[-1])
+
+# print(str1[len(str1)])
+print(str3[1:3])
+print(str3[:3])
+
+print(str3[:-1])
+print(str1[1:])
+print(str4[1:9])
+print(str4[1:9:2])
+print(str4[::-1])
