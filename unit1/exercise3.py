641b3cb88fa872dff8591fa1b8c7f2fb4aa0ca25
Combatd <combatdstar@gmail.com>
2021-06-27 13:17:03 -0700

    Print output of conditionals

---
+++ unit1/exercise3.py
@@ -0,0 +1,35 @@
+if 6 > 7:
+   print("Yep")
+ 
+if 6 > 7:
+   print("Yep")
+else:
+   print("Nope")
+
+temp = 120
+if temp > 85:
+   print("Hot")
+elif temp > 100:
+   print("REALLY HOT!")
+elif temp > 60:
+   print("Comfortable") 
+else:
+   print("Cold")
+
+temp = 50
+if temp > 85:
+   print("Hot")
+elif temp > 100:
+   print("REALLY HOT!")
+elif temp > 60:
+   print("Comfortable")
+else:
+   print("Cold")
+
+var = 'Panda'
+if var == "panda":
+   print("Cute!")
+elif var == "Panda":
+   print("Regal!")
+else:
+   print("Ugly...")
