d9646a1a9a555d5729ab179aa643d064f84731d2
Combatd <combatdstar@gmail.com>
2021-07-17 20:10:53 -0700

    Loop with number increements and decrements

--- unit1/exercise4.py
+++ unit1/exercise4.py
@@ -11,7 +11,7 @@
 # while numberOfLoops < 10:
 #     numberOfApples *= 2
 #     numberOfApples += numberOfLoops
-#     numberOfLoops += 1 // changed to increment instead of decrement
+#     numberOfLoops += 1  # changed to increment instead of decrement
 
 # print("Number of apples: " + str(numberOfApples))
 
@@ -19,3 +19,18 @@ num = 10
 while num > 3:
     num -= 1
     print(num) 
+
+num = 10
+while True:
+    if num < 7:
+        print('Breaking out of loop')
+        break
+    print(num)
+    num -= 1
+print('Outside of loop')
+
+# num = 100
+# while not False:
+#     if num < 0:
+#         break
+# print('num is: ' + str(num)) # this will not execute correctly
