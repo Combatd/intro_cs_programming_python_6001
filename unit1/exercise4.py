6ef8cd2a684cc6380e03dca6c9c2074f32341577
Combatd <combatdstar@gmail.com>
2021-07-17 20:04:26 -0700

    Increment number of loops"

--- unit1/exercise4.py
+++ unit1/exercise4.py
@@ -1,7 +1,16 @@
-num = 0
-while num <= 5:
-    print(num)
-    num += 1
+# num = 0
+# while num <= 5:
+#     print(num)
+#     num += 1
 
-print("Outside of loop")
-print(num) 
+# print("Outside of loop")
+# print(num) 
+
+# numberOfLoops = 0
+# numberOfApples = 2
+# while numberOfLoops < 10:
+#     numberOfApples *= 2
+#     numberOfApples += numberOfLoops
+#     numberOfLoops += 1 // changed to increment instead of decrement
+
+# print("Number of apples: " + str(numberOfApples))
