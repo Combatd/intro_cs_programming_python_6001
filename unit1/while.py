e1c9ef0f5ce4b2373de26b034ed1177ba9a4f1d8
Combatd <combatdstar@gmail.com>
2021-07-17 20:19:33 -0700

    Decrement iterator by 2

--- unit1/while.py
+++ unit1/while.py
@@ -10,3 +10,16 @@ while i <= 10:
     print(i)
     i += 2
 print("Goodbye!")
+
+# prints Hello!
+# prints 10
+# prints 8
+# prints 6
+# prints 4
+# prints 2
+
+print("Hello!")
+i = 10
+while i > 0:
+    print(i)
+    i -= 2
