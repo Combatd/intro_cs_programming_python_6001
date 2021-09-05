1a6411cee93842c179e7fcdf4d5e0552a19ccc15
Combatd <combatdstar@gmail.com>
2021-09-04 19:09:21 -0700

    Create incorrect count loop

--- unit2/Exercise1.py
+++ unit2/Exercise1.py
@@ -1,9 +1,17 @@
+# iteration = 0
+# count = 0
+# while iteration < 5:
+#     # the variable 'letter' in the loop stands for every 
+#     # character, including spaces and commas!
+#     for letter in "hello, world": 
+#         count += 1
+#     print("Iteration " + str(iteration) + "; count is: " + str(count))
+#     iteration += 1 
+
 iteration = 0
-count = 0
 while iteration < 5:
-    # the variable 'letter' in the loop stands for every 
-    # character, including spaces and commas!
-    for letter in "hello, world": 
+    count = 0 # count will always reset to 0 at beginning of every iteration
+    for letter in "hello, world":
         count += 1
     print("Iteration " + str(iteration) + "; count is: " + str(count))
     iteration += 1 
